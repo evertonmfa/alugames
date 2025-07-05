45607b18b2fb750992c2fa87d69119783fbba024
Everton Marinho <ever7onmfa@gmail.com>
2025-07-05 00:22:25 -0300

    btn 1 ok

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
-let botao1 = '1'
-function alterarStatus(botao1){
 
-    alterarStatusBotao()
-  
-//alert('Teste ok')
+
+function alterarStatus() {
+
+    alterarStatusBotao('btngame1')
+
+    //alert('Teste ok')
 }
 
-function alterarStatusBotao() {
+function alterarStatusBotao(btn) {
 
-    
-    let botao = document.getElementById('game-1');
-    if (botao.classList.contains('dashboard__item__button')) {
-        alert('tem a class')
-        botao.classList.remove('dashboard__item__button');
-        botao.classList.add('dashboard__item__button dashboard__item__button--return')
-    }   
-    else{
-        botao.classList.remove('dashboard__item__button dashboard__item__button--return');
-        botao.classList.add('dashboard__item__button')
-        alert('Na o tem a class')
+
+    let botao = document.getElementById(btn);
+    if (botao.classList.contains('dashboard__item__button--return')) {
+
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+
+    }
+    else {
+
+        botao.classList.add('dashboard__item__button--return')
+        botao.textContent = 'Devolver';
 
     }
 }
